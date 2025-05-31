7c6ef670de34b23b96a28da18c62a2ab725e03ad
Hendrik Cooper <biocentra.info@gmail.com>
2025-05-31 05:38:08 +0200

    Update BentoStats component: Replace background image with Droplets1, enhance hover effects with new mask and backdrop filters, and improve text styling for better visual impact.

--- src/components/bentos/Gen_3_Bentos/BentoStats.tsx
+++ src/components/bentos/Gen_3_Bentos/BentoStats.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import EnzymeImage from "@/assets/images/enzymeImage.png";
 import WaterRepellantFabric from "@/assets/images/water-repellant-fabric.png";
 import AnotherDarkBGwithDroplets from "@/assets/images/BGImagesTest/AnotherDarkBGwithDroplets.png";
+import Droplets1 from "@/assets/images/Droplets1.jpg";
 
 export function BentoStats({ className }: { className?: string }) {
   return (
@@ -18,43 +19,71 @@ export function BentoStats({ className }: { className?: string }) {
     >
       {/* <Image src={EnzymeImage} alt="Enzyme Image" className="w-full h-full object-cover absolute top-0 left-0" /> */}
       <motion.div
-        className="absolute top-0 left-0 w-full h-full"
+        className="absolute top-0 left-0 w-full h-full "
         initial={{
           scale: 1,
           y: 0,
         }}
         variants={{
           hover: { 
-            scale: 2,
-            y: 60,
+            scale: 1.2,
+            y: -10,
            },
         }}
         transition={{ duration: 0.2 }}
       >
         <Image
-          src={AnotherDarkBGwithDroplets}
+          src={Droplets1}
           alt="Water Repellant Fabric"
           className="w-full h-full object-cover"
         />
       </motion.div>
       <motion.div
-        className="absolute top-0 left-0 w-full h-full"
-        initial={{ backgroundImage:
-          "radial-gradient(circle at center, rgba(0,0,0,0.3) 0%, rgba(0,0,0,0.4) 50%, rgba(0,0,0,0..5) 100%)"}}
+        className="absolute top-0 left-0 w-full h-full "
+        initial={{ 
+          maskImage: "linear-gradient(to right, black 0%, black 30%, transparent 100%)",
+          backdropFilter: "blur(4px)",
+        }}
         variants={{
           hover: {
-            backgroundImage:
-              "radial-gradient(circle at center, rgba(0,0,0,0) 0%, rgba(0,0,0,0) 50%, rgba(0,0,0,0) 100%)",
+            maskImage: "linear-gradient(to right, black 0%, black 30%, transparent 100%)",
+            backdropFilter: "blur(2px)",
           },
         }}
         transition={{ duration: 0.2 }}
       />
-      <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-t from-black/60 via-transparent to-transparent "></div>
-      <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-black/60 via-transparent to-transparent "></div>
-      <div className="p-4  absolute top-0 left-0 w-full h-full">
-        <h1 className="text-3xl font-bold bg-lightGrey/90 text-transparent bg-clip-text text-left w-full tracking-wider">
-          PFAS can be found in a wide range of products:
-        </h1>
+      {/* <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-t from-black/60 via-transparent to-transparent "></div>
+      <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-black/60 via-transparent to-transparent "></div> */}
+      <div className="p-4  absolute top-0 left-0 w-full h-full bg-mintAccent/10">
+        <motion.h1 
+          initial={{ opacity: 0, scale: 1.3 }}
+          variants={{
+            hover: {
+              opacity: 1,
+              scale: 1,
+            },
+          }}
+          viewport={{ once: true }}
+          className="text-2xl leading-7 font-semibold bg-gradient-to-r bg-slate-600 from-tealAccent/30 to-tealAccent/20 text-transparent bg-clip-text text-left w-full tracking-wider"
+        >
+          <span className="font-bold text-3xl">PFAS</span> <br/>can be found in countless materials and products
+        </motion.h1>
+      <motion.div className="absolute top-0 left-0 w-full h-full flex items-center justify-center"
+          initial={{
+            opacity: 1, 
+          }}
+          variants={{
+            hover: {
+              opacity: 0,
+            },
+          }}
+          >
+          <h1 className="text-8xl font-bold font-poppins 
+          bg-gradient-to-br bg-slate-800 from-mintAccent/50 via-tealAccent/70 to-tealAccent/20 text-transparent bg-clip-text
+           text-center">
+            PFAS
+          </h1>
+        </motion.div>
       </div>
     </motion.div>
   );
